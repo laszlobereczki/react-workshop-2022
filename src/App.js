44b5c13815c1353f213400c10bca8e1a79bced0d
laszlobereczki <laszlo.bereczki@liferay.com>
2022-05-05 15:19:19 +0200

    squash commit of first week
    due to git related issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [inputValue, setInputValue] = useState(0);
+
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <Amount value={inputValue} message="Euro value: "/>
+        <Amount value={exchangeRate(inputValue)} message="BTC value: "/>
+        <input defaultValue={0} type='number' label="Euros"
+               onChange={(event) => setInputValue(Number(event.target.value))}/>
+      </div>
   );
+}
+
+function Amount(props) {
+  const myStyle = (props.value < 0) ? {color: "red"} : {color: "black"}
+  return <p style={myStyle}>{props.message}{props.value}</p>;
+}
+
+function exchangeRate(euroValue) {
+  return (euroValue * 0.000026).toFixed(6);
 }
 
 export default App;
